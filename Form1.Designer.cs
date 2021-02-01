1b7a6a92b58520b21916e6dfe0c62b84fb876d4c
Robinson728 <77537741+Robinson728@users.noreply.github.com>
2021-02-01 17:56:30 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,377 @@
+﻿
+namespace RegistroUsuarios
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txt_email = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_nombre = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_alias = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_eliminar = new System.Windows.Forms.Button();
+            this.btn_guardar = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.btn_buscar = new System.Windows.Forms.Button();
+            this.IdnumericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.txt_clave = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txt_confirmar = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txt_costohora = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.IdnumericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(589, 12);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(103, 23);
+            this.dateTimePicker1.TabIndex = 47;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(497, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 15);
+            this.label3.TabIndex = 45;
+            this.label3.Text = "Rol Id";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(489, 15);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(96, 15);
+            this.label6.TabIndex = 44;
+            this.label6.Text = "Fecha de Ingreso";
+            // 
+            // txt_email
+            // 
+            this.txt_email.Location = new System.Drawing.Point(450, 150);
+            this.txt_email.Name = "txt_email";
+            this.txt_email.Size = new System.Drawing.Size(242, 23);
+            this.txt_email.TabIndex = 54;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(408, 156);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 15);
+            this.label5.TabIndex = 53;
+            this.label5.Text = "Email";
+            // 
+            // txt_nombre
+            // 
+            this.txt_nombre.Location = new System.Drawing.Point(96, 150);
+            this.txt_nombre.Name = "txt_nombre";
+            this.txt_nombre.Size = new System.Drawing.Size(278, 23);
+            this.txt_nombre.TabIndex = 51;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(31, 153);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 15);
+            this.label4.TabIndex = 49;
+            this.label4.Text = "Nombres";
+            // 
+            // txt_alias
+            // 
+            this.txt_alias.Location = new System.Drawing.Point(96, 108);
+            this.txt_alias.Name = "txt_alias";
+            this.txt_alias.Size = new System.Drawing.Size(226, 23);
+            this.txt_alias.TabIndex = 56;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(31, 111);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(32, 15);
+            this.label7.TabIndex = 55;
+            this.label7.Text = "Alias";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 15);
+            this.label1.TabIndex = 58;
+            this.label1.Text = "Usuario Id";
+            // 
+            // btn_eliminar
+            // 
+            this.btn_eliminar.Image = ((System.Drawing.Image)(resources.GetObject("btn_eliminar.Image")));
+            this.btn_eliminar.Location = new System.Drawing.Point(437, 241);
+            this.btn_eliminar.Name = "btn_eliminar";
+            this.btn_eliminar.Size = new System.Drawing.Size(106, 63);
+            this.btn_eliminar.TabIndex = 61;
+            this.btn_eliminar.Text = "Eliminar";
+            this.btn_eliminar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btn_eliminar.UseVisualStyleBackColor = true;
+            this.btn_eliminar.Click += new System.EventHandler(this.btn_eliminar_Click);
+            // 
+            // btn_guardar
+            // 
+            this.btn_guardar.Image = ((System.Drawing.Image)(resources.GetObject("btn_guardar.Image")));
+            this.btn_guardar.Location = new System.Drawing.Point(293, 241);
+            this.btn_guardar.Name = "btn_guardar";
+            this.btn_guardar.Size = new System.Drawing.Size(106, 63);
+            this.btn_guardar.TabIndex = 64;
+            this.btn_guardar.Text = "Guardar";
+            this.btn_guardar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btn_guardar.UseVisualStyleBackColor = true;
+            this.btn_guardar.Click += new System.EventHandler(this.btn_guardar_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // btn_buscar
+            // 
+            this.btn_buscar.Image = ((System.Drawing.Image)(resources.GetObject("btn_buscar.Image")));
+            this.btn_buscar.Location = new System.Drawing.Point(169, 57);
+            this.btn_buscar.Name = "btn_buscar";
+            this.btn_buscar.Size = new System.Drawing.Size(41, 36);
+            this.btn_buscar.TabIndex = 65;
+            this.btn_buscar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btn_buscar.UseVisualStyleBackColor = true;
+            this.btn_buscar.Click += new System.EventHandler(this.btn_buscar_Click);
+            // 
+            // IdnumericUpDown1
+            // 
+            this.IdnumericUpDown1.Location = new System.Drawing.Point(96, 59);
+            this.IdnumericUpDown1.Name = "IdnumericUpDown1";
+            this.IdnumericUpDown1.Size = new System.Drawing.Size(58, 23);
+            this.IdnumericUpDown1.TabIndex = 66;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(540, 59);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(58, 23);
+            this.numericUpDown1.TabIndex = 67;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(632, 60);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(60, 19);
+            this.checkBox1.TabIndex = 68;
+            this.checkBox1.Text = "Activo";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Presidente",
+            "Director",
+            "Administrador 1",
+            "Administrador 2",
+            "Administrador 3",
+            "Contador 1",
+            "Contador 2",
+            "Contador 3",
+            "Coordinador",
+            "Secretaria/o"});
+            this.comboBox1.Location = new System.Drawing.Point(516, 108);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(176, 23);
+            this.comboBox1.TabIndex = 69;
+            // 
+            // txt_clave
+            // 
+            this.txt_clave.Location = new System.Drawing.Point(96, 190);
+            this.txt_clave.Name = "txt_clave";
+            this.txt_clave.Size = new System.Drawing.Size(134, 23);
+            this.txt_clave.TabIndex = 71;
+            this.txt_clave.TextChanged += new System.EventHandler(this.txt_clave_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 193);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 15);
+            this.label2.TabIndex = 70;
+            this.label2.Text = "Clave";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(486, 111);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(24, 15);
+            this.label8.TabIndex = 72;
+            this.label8.Text = "Rol";
+            // 
+            // button1
+            // 
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(149, 241);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 63);
+            this.button1.TabIndex = 73;
+            this.button1.Text = "Nuevo";
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txt_confirmar
+            // 
+            this.txt_confirmar.Location = new System.Drawing.Point(348, 190);
+            this.txt_confirmar.Name = "txt_confirmar";
+            this.txt_confirmar.Size = new System.Drawing.Size(134, 23);
+            this.txt_confirmar.TabIndex = 75;
+            this.txt_confirmar.TextChanged += new System.EventHandler(this.txt_confirmar_TextChanged);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(249, 193);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(93, 15);
+            this.label9.TabIndex = 74;
+            this.label9.Text = "Confirmar Clave";
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // txt_costohora
+            // 
+            this.txt_costohora.Location = new System.Drawing.Point(599, 190);
+            this.txt_costohora.Name = "txt_costohora";
+            this.txt_costohora.Size = new System.Drawing.Size(93, 23);
+            this.txt_costohora.TabIndex = 77;
+            this.txt_costohora.TextChanged += new System.EventHandler(this.txt_costohora_TextChanged);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(516, 193);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(77, 15);
+            this.label10.TabIndex = 76;
+            this.label10.Text = "Costo X Hora";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(723, 316);
+            this.Controls.Add(this.txt_costohora);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.txt_confirmar);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txt_clave);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.IdnumericUpDown1);
+            this.Controls.Add(this.btn_buscar);
+            this.Controls.Add(this.btn_guardar);
+            this.Controls.Add(this.btn_eliminar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_alias);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txt_email);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txt_nombre);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label6);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Registro de Usuario";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.IdnumericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txt_email;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txt_nombre;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_alias;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_eliminar;
+        private System.Windows.Forms.Button btn_guardar;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.Button btn_buscar;
+        private System.Windows.Forms.NumericUpDown IdnumericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txt_clave;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txt_confirmar;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txt_costohora;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RegistroUsuarios.Entidades;
+using RegistroUsuarios.BLL;
+using RegistroUsuarios.DAL;
+
+namespace RegistroUsuarios
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Limpiar()
+        {
+            IdnumericUpDown1.Value = 0;
+            txt_alias.Text = string.Empty;
+            txt_email.Text = string.Empty;
+            txt_nombre.Text = string.Empty;
+            txt_clave.Text = string.Empty;
+            comboBox1.Text = string.Empty;
+            txt_confirmar.Text = string.Empty;
+            txt_costohora.Text = string.Empty;
+            numericUpDown1.Value = 0;
+            dateTimePicker1.Value = DateTime.Now;
+            errorProvider1.Clear();
+        }
+
+        private void LlenaCampo(Persona personas)
+        {
+            
+            IdnumericUpDown1.Value = personas.UsuarioId;
+            txt_nombre.Text = personas.Nombre;
+            txt_alias.Text = personas.Alias;
+            txt_email.Text = personas.Email;
+            txt_clave.Text = personas.Clave;
+            comboBox1.Text = personas.Rol;
+            txt_confirmar.Text = personas.ConfirmarClave;
+            txt_costohora.Text = personas.Costo;
+            numericUpDown1.Value = personas.RolId;
+            dateTimePicker1.Value = personas.FechaIngreso;
+            checkBox1.Checked = personas.Activo;
+        }
+
+        private Persona LlenaClase()
+        {
+            Persona personas = new Persona();
+            personas.UsuarioId = (int)IdnumericUpDown1.Value;
+            personas.Nombre = txt_nombre.Text;
+            personas.Email = txt_email.Text;
+            personas.Alias = txt_alias.Text;
+            personas.Clave = txt_clave.Text;
+            personas.ConfirmarClave = txt_confirmar.Text;
+            personas.Costo = txt_costohora.Text;
+            personas.Rol = comboBox1.Text;
+            personas.RolId = (int)numericUpDown1.Value;
+            personas.FechaIngreso = dateTimePicker1.Value;
+            personas.Activo = checkBox1.Checked;
+
+            return personas;
+        }
+        private bool ExisteEnLaBaseDeDatos()
+        {
+            Persona personas = PersonaBLL.Buscar((int)IdnumericUpDown1.Value);
+
+            return (personas != null);
+        }
+
+        private bool Validar()
+        {
+            bool paso = true;
+            errorProvider1.Clear();
+            string cadena = "";
+            string cadena2 = "";
+
+            cadena = txt_clave.Text;
+            cadena2 = txt_confirmar.Text;
+
+            if(txt_alias.Text == string.Empty)
+            {
+                errorProvider1.SetError(txt_alias, "El campo nombre no puede estar vacio");
+                txt_alias.Focus();
+                paso = false;
+            }
+            else if (txt_nombre.Text == string.Empty)
+            {
+                errorProvider1.SetError(txt_nombre, "El campo Alias no puede estar vacio");
+                txt_nombre.Focus();
+                paso = false;
+            }
+            else if (txt_clave.Text == string.Empty)
+            {
+                errorProvider1.SetError(txt_clave, "El campo Email no puede estar vacio");
+                txt_clave.Focus();
+                paso = false;
+            }
+            else if (txt_confirmar.Text == string.Empty)
+            {
+                errorProvider1.SetError(txt_confirmar, "El campo Alias no puede estar vacio");
+                txt_confirmar.Focus();
+                paso = false;
+            }
+            else if (txt_email.Text == string.Empty)
+            {
+                errorProvider1.SetError(txt_email, "El campo Alias no puede estar vacio");
+                txt_email.Focus();
+                paso = false;
+            }
+            else if (txt_costohora.Text == string.Empty)
+            {
+                errorProvider1.SetError(txt_costohora, "El campo Alias no puede estar vacio");
+                txt_costohora.Focus();
+                paso = false;
+            }
+            else if(string.Equals(cadena, cadena2) == true)
+            {
+                paso = true;
+            }
+            else
+            {
+                errorProvider1.SetError(txt_confirmar, "La clave es distinta");
+                txt_confirmar.Focus();
+                paso = false;
+            }
+
+            return paso;
+        }
+
+        private void dataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void txt_descripcion_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_eliminar_Click(object sender, EventArgs e)
+        {
+            errorProvider1.Clear();
+
+            int id;
+            int.TryParse(IdnumericUpDown1.Text, out id);
+
+            Limpiar();
+
+            if (PersonaBLL.Eliminar(id))
+                MessageBox.Show("Eliminado", "Exito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else
+                errorProvider1.SetError(IdnumericUpDown1, "Id no existente");
+        }
+
+        private void btn_guardar_Click(object sender, EventArgs e)
+        {
+            Persona personas;
+            bool paso = false;
+
+            if (!Validar())
+                return;
+
+            personas = LlenaClase();
+
+            paso = PersonaBLL.Guardar(personas);
+
+            if (paso)
+            {
+                Limpiar();
+                MessageBox.Show("Guardado!!", "Correcto", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("No fue posible guardar, Id en existencia", "Fallo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btn_buscar_Click(object sender, EventArgs e)
+        {
+            int id;
+            Persona personas = new Persona();
+            int.TryParse(IdnumericUpDown1.Text, out id);
+
+            Limpiar();
+
+            personas = PersonaBLL.Buscar(id);
+
+            if (personas != null)
+            {
+                LlenaCampo(personas);
+            }
+            else
+            {
+                MessageBox.Show("Persona no encontrada", "Id Incorrecto", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Limpiar();
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_confirmar_TextChanged(object sender, EventArgs e)
+        {
+            txt_confirmar.PasswordChar = '*';
+        }
+
+        private void txt_costohora_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_clave_TextChanged(object sender, EventArgs e)
+        {
+            txt_clave.PasswordChar = '*';
+        }
+    }
+}
